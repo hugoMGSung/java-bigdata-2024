b3187885bf06a2c8d3169b996698dbadd59c73a3
Hugo MG Sung <personar95@naver.com>
2024-02-27 15:28:03 +0900

    이미지 업로드

--- day05/p34_addrBook.py
+++ day05/p34_addrBook.py
@@ -39,7 +39,8 @@ def setContact(): # 사용자 입력으로 주소록 받기함수
     return contact
 
 def delContact(lst, name): # 연락처 삭제함수
-    for i, item in enumerate(lst):
+    for i in range(len(lst)-1, -1, -1): # 리스트를 내림차순으로 뒤에서부터 삭제
+        item = lst[i]
         if item.isNameExist(name):
             del lst[i]
 
@@ -70,7 +71,11 @@ def displayMenu():
             '3. 연락처 삭제\n'
             '4. 종료\n')
     print(menu)
-    sel = int(input('메뉴입력 : '))
+    try:
+        sel = int(input('메뉴입력 : '))
+    except: # 1~4가 아닌 잘못된 문자 입력할때 예외처리
+        sel = 0
+
     return sel
 
 def clearConsole():
@@ -96,10 +101,18 @@ def run():
 
         if selMenu == 1: # 연락처 추가라면
             clearConsole()
-            contact = setContact()
-            lstContact.append(contact)
-            # print(lstContact)
-            input('입력 성공!'); clearConsole() # 엔터 입력유도            
+            try:    
+                contact = setContact()
+            except: # 입력을 시킨대로 안하면 
+                contact = None
+
+            if contact != None:
+                lstContact.append(contact)
+                input('입력 성공!')
+            else:
+                input('입력 실패')
+
+            clearConsole() # 엔터 입력유도            
         elif selMenu == 2: # 연락처 출력
             clearConsole()
             getContacts(lstContact)
